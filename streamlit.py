0dfb1e8e743dd16f93f3e8180bce57141a141031
libella <ukhanovait@gmail.com>
2024-09-16 21:28:48 +0200

    v2

--- streamlit.py
+++ streamlit.py
@@ -11,10 +11,10 @@ st.subheader('Irina Ukhanova')
 st.subheader('Ist diese Nachricht Spam oder nicht?')
 st.markdown('Probieren Sie dieses ML-Modell aus, um zu prüfen, ob die Nachricht auf den P4G-Portalen als Spam markiert werden sollte.')
 with st.form("my_form"):
-	email = st.text_input("E-Mail")
-	txt = st.text_area("Geben Sie hier die Nachricht an (nicht mehr als 1000 Zeichen):", max_chars=1000)
+	mail = st.text_input("E-Mail")
+	message = st.text_area("Geben Sie hier die Nachricht an (nicht mehr als 1000 Zeichen):", max_chars=1000)
 	if st.form_submit_button("Spam oder nicht?"):
-		result = predict(np.array([[email, txt]]))
+		result = predict(np.array([[mail, message]]))
 		st.text(result[0])
 	#submitted = st.form_submit_button("Spam oder nicht?")
 	#if submitted:
