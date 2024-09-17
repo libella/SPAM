0f640cfa07b99dd8979c076fa1c92dc53df847f8
Irina Ukhanova <ukhanovait@gmail.com>
2024-09-17 21:06:05 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,18 +2,32 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from prediction import predict
 
-#data = pd.DataFrame()
+# Funktion zum Laden des Modells
+def load_modelR():
+    with open('spam_modelR.joblib', 'rb') as file:
+        modelR = joblib.load(file)
+    return modelR
+
+modelR = load_modelR()
+
+# Nutzer-Eingaben
 st.title('Spam-Projekt, IKT-SS24')
 st.subheader('Irina Ukhanova')
 
 st.subheader('Ist diese Nachricht Spam oder nicht?')
 st.markdown('Probieren Sie dieses ML-Modell aus, um zu prüfen, ob die Nachricht auf den P4G-Portalen als Spam markiert werden sollte.')
-with st.form("my_form"):
+def user_input():
 	message = st.text_area("Geben Sie hier die Nachricht an (nicht mehr als 1000 Zeichen):", max_chars=1000)
-	if st.form_submit_button("Spam oder nicht?"):
-		result = predict(np.array([[message]]))
-		st.text(result[0])
-	#new_data = dp.data_preprocessing(data=data)
-	#st.sucess("Die Nachricht ist {}".format(predict(new_data)))
+	data = {
+        'message': message
+	}
+	return np.array(list(data.values()))
+input_data = user_input()
+
+# Vorhersage
+prediction = modelR.predict(input_data)
+
+# Vorhersage-Ergebnis anzeigen
+st.write(f'Die Nachricht ist: **{prediction[0]}**')
+
