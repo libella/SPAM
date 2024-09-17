1afced9df51c0bbed90a3ff70715ceaa4342ccd0
Irina Ukhanova <ukhanovait@gmail.com>
2024-09-17 21:13:48 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,33 +1,43 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 import joblib
 
-# Funktion zum Laden des Modells
-def load_modelR():
-    with open('spam_modelR.joblib', 'rb') as file:
-        modelR = joblib.load(file)
-    return modelR
+# Lade das trainierte Modell und den Vektorisierer
+model = joblib.load('spam_modelR.joblib')
+vectorizer = joblib.load('vectorizer.joblib')
 
-modelR = load_modelR()
-
-# Nutzer-Eingaben
+# Titel und Beschreibung der App
 st.title('Spam-Projekt, IKT-SS24')
 st.subheader('Irina Ukhanova')
 
 st.subheader('Ist diese Nachricht Spam oder nicht?')
-st.markdown('Probieren Sie dieses ML-Modell aus, um zu prüfen, ob die Nachricht auf den P4G-Portalen als Spam markiert werden sollte.')
-def user_input():
-	message = st.text_area("Geben Sie hier die Nachricht an (nicht mehr als 1000 Zeichen):", max_chars=1000)
-	data = {
-        'message': message
-	}
-	return np.array(list(data.values()))
-input_data = user_input()
+st.write('Probieren Sie dieses ML-Modell aus, um zu prüfen, ob die Nachricht auf den P4G-Portalen als Spam markiert werden sollte.')
 
-# Vorhersage
-prediction = modelR.predict(input_data)
+# Text-Eingabe vom Nutzer
+input_text = st.text_area('Nachricht eingeben:')
 
-# Vorhersage-Ergebnis anzeigen
-st.write(f'Die Nachricht ist: **{prediction[0]}**')
+if st.button('Überprüfen'):
+    if input_text:
+        # Verarbeite die Nutzereingabe
+        input_vector = vectorizer.transform([input_text])
+        
+        # Vorhersage
+        prediction = model.predict(input_vector)
+        
+        # Zeige das Ergebnis an
+        if prediction == 1:
+            st.error('Diese Nachricht ist **Spam**.')
+        else:
+            st.success('Diese Nachricht ist **kein Spam**.')
+    else:
+        st.warning('Bitte geben Sie eine Nachricht ein.')
+
+
+
+
+
+
+
+
+
+
 
